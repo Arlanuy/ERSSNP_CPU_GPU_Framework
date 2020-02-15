66626144f01b421b5833c298097771dff517f182
Jeremy Wu <wujeremy361@gmail.com>
2020-02-16 03:44:20 +0800

    add files

---
+++ tests_wu/thesis_a4.py
@@ -0,0 +1,107 @@
+import pycuda.autoinit
+import pycuda.driver as drv
+import numpy
+
+from pycuda.compiler import SourceModule
+
+def LCSubStr(X, Y, m, n): 
+    
+    # Create a table to store lengths of 
+    # longest common suffixes of substrings. 
+    # Note that LCSuff[i][j] contains the 
+    # length of longest common suffix of 
+    # X[0...i-1] and Y[0...j-1]. The first 
+    # row and first column entries have no 
+    # logical meaning, they are used only 
+    # for simplicity of the program. 
+    
+    # LCSuff is the table with zero 
+    # value initially in each cell 
+    LCSuff = [[0 for k in range(n+1)] for l in range(m+1)] 
+    
+    # To store the length of 
+    # longest common substring 
+    result = 0
+
+    # Following steps to build 
+    # LCSuff[m+1][n+1] in bottom up fashion 
+    for i in range(m + 1): 
+        for j in range(n + 1): 
+            if (i == 0 or j == 0): 
+                LCSuff[i][j] = 0
+            elif (X[i-1] == Y[j-1]): 
+                LCSuff[i][j] = LCSuff[i-1][j-1] + 1
+                result = max(result, LCSuff[i][j]) 
+            else: 
+                LCSuff[i][j] = 0
+    for a in LCSuff:
+        print(a)
+    return result 
+
+
+mod = SourceModule("""
+#include <stdlib.h>
+__device__ int max1(int a,int b){
+    if(a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+__global__ void lc_substring(int *X,int *Y,int *res, int *LCSuff,int row_width,int col_width){
+   int j = blockIdx.x * blockDim.x + threadIdx.x;
+   if (j < col_width){
+        for (int i = 0; i < row_width; i++) {
+            if (X[i-1] == Y[j-1] && !(i == 0 || j == 0)) {  
+                LCSuff[i*col_width+j] = LCSuff[(i-1)*col_width+j-1] + 1;
+                __syncthreads();
+                //printf("compare %d %d\\n",res[0],LCSuff[i*col_width+j]);
+                res[0] = max1(res[0], LCSuff[i*col_width+j]);
+                __syncthreads();
+                //printf("res %d %d %d\\n",res[0],i,j);
+                
+            } 
+            else{
+                LCSuff[i*col_width+j] = 0;
+                __syncthreads();
+            }
+            
+        }
+
+    } 
+}
+
+
+""")
+LCS = mod.get_function("lc_substring")
+no = [1,1,1,0,1,0,1]
+a = numpy.array(no,dtype=numpy.int32) #row the width
+b = numpy.array([0,0,0,0,0,1],dtype=numpy.int32) #col
+res = numpy.array([0],dtype=numpy.int32)
+LCSuff = numpy.zeros((a.size+1,b.size+1),dtype=numpy.int32)
+
+print(LCSuff[0])
+
+a_gpu = drv.mem_alloc(a.size * a.dtype.itemsize)
+b_gpu = drv.mem_alloc(b.size * b.dtype.itemsize)
+LCSuff_gpu = drv.mem_alloc(LCSuff.size * LCSuff.dtype.itemsize)
+res_gpu = drv.mem_alloc(res.size * res.dtype.itemsize)
+
+drv.memcpy_htod(a_gpu, a)
+drv.memcpy_htod(b_gpu, b)
+drv.memcpy_htod(LCSuff_gpu, LCSuff)
+drv.memcpy_htod(res_gpu, res)
+
+
+LCS(a_gpu,b_gpu,res_gpu,LCSuff_gpu, numpy.int32(a.size+1),numpy.int32(b.size+1), block=(10,10,1),grid=(1,1,1))
+drv.memcpy_dtoh(LCSuff, LCSuff_gpu)
+drv.memcpy_dtoh(res, res_gpu)
+
+print(LCSuff)
+print(res)
+print(LCSubStr(a, b, len(a), len(b))) 
+
+
+#need same length to be consistent
+#make the first param the shorter one
--- tests_wu/yaml_test_1.py
+++ tests_wu/yaml_test_1.py
@@ -6,17 +6,30 @@ def conf_load(filename):
 
 			#getting generation zero from run zero
 			generation_zero = ga_params['runs'][0]['generations'][0]
-			print(generation_zero)
+			# print(generation_zero)
 
 			#getting rssnp index 0 in generation zero
 			rssnp_zero = generation_zero['rssnp_chromosomes'][0]
-			print(rssnp_zero) 
+			# print(rssnp_zero) 
 
 			#getting out_pairs indexed 0 at rssnp indexed 0 in generation zero
 			out_pairs_zero = rssnp_zero['out_pairs'][0]
-			print(out_pairs_zero)
+			# print(out_pairs_zero)
+			return generation_zero, rssnp_zero, out_pairs_zero
 		except yaml.YAMLError as exc:
 			print(exc)
 
 filename = 'ga_conf_out.yaml'
-conf_load(filename)
+generation ,rssnp , out_pairs = conf_load(filename)
+
+print(generation)
+print('\n')
+print(rssnp)
+print(out_pairs)
+
+for a in rssnp:
+	print(a)
+
+for a in out_pairs:
+	print(a)
+
