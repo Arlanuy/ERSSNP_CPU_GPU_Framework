b041d774541bb1540c1c42e357dc4ea0dab3ea7f
Jeremy Wu <wujeremy361@gmail.com>
2020-02-15 17:54:15 +0800

    Create yaml_test_1.py
    added a test file

---
+++ tests_wu/yaml_test_1.py
@@ -0,0 +1,22 @@
+import yaml
+def conf_load(filename):
+	with open(filename, 'r') as stream:
+		try:
+			ga_params = yaml.safe_load(stream)
+
+			#getting generation zero from run zero
+			generation_zero = ga_params['runs'][0]['generations'][0]
+			print(generation_zero)
+
+			#getting rssnp index 0 in generation zero
+			rssnp_zero = generation_zero['rssnp_chromosomes'][0]
+			print(rssnp_zero) 
+
+			#getting out_pairs indexed 0 at rssnp indexed 0 in generation zero
+			out_pairs_zero = rssnp_zero['out_pairs'][0]
+			print(out_pairs_zero)
+		except yaml.YAMLError as exc:
+			print(exc)
+
+filename = 'ga_conf_out.yaml'
+conf_load(filename)
