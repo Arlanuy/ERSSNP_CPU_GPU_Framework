5f7f59ae3f0fa0e416a2e2da21f9d23a759fe0ea
Arlan Uy <gnhs.com@gmail.com>
2021-07-04 06:15:09 +0800

    revise timer name

--- main_dir/src/abstracts/cpu_timer.py
+++ main_dir/src/abstracts/cpu_timer.py
@@ -1,6 +1,6 @@
 import os
 
-name = "test_cpu"
+name = "cpuandminimal00outreal.txt"
 
 def timer_write(ga_name, start, finish):
     timer_out_cpu = open(os.getcwd()+ "\\timer_directory\\" + name, "a+")
